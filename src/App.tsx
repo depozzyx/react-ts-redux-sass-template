3d96839b9fc0857e11770b50e670305595e6a8b2
depozzyx <mosentsowivan@gmail.com>
2021-02-25 21:05:50 +0200

    Styled components adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import styled, { ThemeProvider } from "styled-components";
 import {
     incrementCounter,
     decrementCounter,
     changeIsLogged,
 } from "./redux/actions";
+
+const Card = styled.div`
+    max-width: 350px;
+    border: 3px solid rgba(120, 248, 61, 0.3);
+    padding: 7px;
+    border-radius: 5px;
+    overflow: hidden;
+    box-shadow: 5px 5px 10px rgba(120, 248, 61, 0.05);
+    margin: 30px auto;
+`;
 
 function App() {
     const counter = useSelector((state: any) => state.counter);
@@ -14,6 +25,7 @@ function App() {
     return (
         <div className="App">
             <h1>Hello! Counter = {counter}</h1>
+            <Card>Hi bro! Well cum!</Card>
             <button
                 onClick={() => {
                     dispatch(incrementCounter());
